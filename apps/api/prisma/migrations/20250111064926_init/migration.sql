e55ca48395b8c724b9e52e64eddbb6d6bed09ec2
numdinkushi <numdinkushi@gmail.com>
2025-01-11 07:51:40 +0100

    set up prisma, prosgresQl db

---
+++ apps/api/prisma/migrations/20250111064926_init/migration.sql
@@ -0,0 +1,70 @@
+-- CreateEnum
+CREATE TYPE "ProductStatus" AS ENUM ('MANUFACTURED', 'SOLD', 'RETURNED', 'RECYCLED');
+
+-- CreateTable
+CREATE TABLE "Manufacturer" (
+    "id" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "contact" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "manufacturerId" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "ToxicItem" (
+    "id" SERIAL NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "ToxicItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductItem" (
+    "id" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "productId" TEXT NOT NULL,
+    "status" "ProductStatus" NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "productItemId" TEXT,
+    "status" "ProductStatus" NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Manufacturer_id_key" ON "Manufacturer"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_id_key" ON "Product"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProductItem_id_key" ON "ProductItem"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transaction_id_key" ON "Transaction"("id");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_manufacturerId_fkey" FOREIGN KEY ("manufacturerId") REFERENCES "Manufacturer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ToxicItem" ADD CONSTRAINT "ToxicItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductItem" ADD CONSTRAINT "ProductItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_productItemId_fkey" FOREIGN KEY ("productItemId") REFERENCES "ProductItem"("id") ON DELETE SET NULL ON UPDATE CASCADE;
