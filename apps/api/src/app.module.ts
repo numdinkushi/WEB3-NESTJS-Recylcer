d86be0daf781ed3f22b3b80754f012842ab2c4ca
numdinkushi <numdinkushi@gmail.com>
2025-01-16 12:09:23 +0100

    wip: event listeners

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { PrismaModule } from './common/prisma/prisma.module';
-import { ListenerModule } from './listener/listener.module';
+import { PrismaModule } from './common/prisma/prisma.module'
+import { ListenerModule } from './listener/listener.module'
 
 @Module({
   imports: [PrismaModule, ListenerModule],
--- apps/api/src/common/utils.ts
+++ apps/api/src/common/utils.ts
@@ -1,1 +1,1 @@
-export const contractAddress = '0x405d3BA3Fad967b3a96e1952d140FeF9F71093a9';
+export const contractAddress = '0x405d3BA3Fad967b3a96e1952d140FeF9F71093a9'
